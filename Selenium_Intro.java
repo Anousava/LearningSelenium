be59623624cd1c2c9af0de377e408c24fa8cb626
Anousava <g.ana_04@hotmail.com>
2020-03-05 21:59:25 +0100

    First commit

---
+++ Selenium_Intro.java
@@ -0,0 +1,23 @@
+package Predavanja;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_Intro {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ana\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.coolinarika.com");
+		//driver.navigate().to("https://www.coolinarika.com");
+		//driver.navigate().back();
+		//driver.navigate().forward();
+		driver.findElement(By.id("fm_text_search")).sendKeys("torta");
+		driver.findElement(By.id("fm_text_search")).sendKeys(Keys.ENTER);
+		
+	}
+
+}
